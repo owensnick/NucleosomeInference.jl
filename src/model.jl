14baca90d3f9fac3fc166dac8beef7c3bb11273b
Nick Owens <owensnick@gmail.com>
2021-07-09 15:40:15 +0100

    dependencies, model, sampling

---
+++ src/model.jl
@@ -0,0 +1,22 @@
+
+### log pdf of a multivariate normal
+function logpfun_mv(x, μ, σ, w)
+    logw = log.(w)
+    K = length(logw)
+    N = size(x, 2)
+    sum(logsumexp(logw[k] + logpdf(MvNormal(μ[k], σ), x[:,n]) for k in 1:K) for n in 1:N)
+end
+
+
+@model function simplemulticlass(x, nc, nsites, σ,  p,  wc=ones(nc)/nc, wp=ones(nsites-1)/(nsites-1), w=wc.*wp', q = 2)
+    
+    n = size(x, 2)
+    startpos ~ filldist(Uniform(-1500, 1500), nc)
+    delta    ~ filldist(Gamma(q*p, 1/q), nc, nsites)
+    configs = startpos .+ cumsum(delta, dims=2)
+    
+    μ = [[configs[i, j], configs[i, j+1]] for i = 1:nc, j=1:(nsites-1)]
+    
+    #x ~ filldist(MixtureModel(Normal.(vec(μ), w)), n)
+    Turing.@addlogprob! logpfun_mv(x, vec(μ), σ, w)
+end
---
+++ src/sampling.jl
@@ -0,0 +1,6 @@
+
+function sample_mh_jump(model, d=180)
+
+    mh = MH(:startpos => x -> [MixtureModel(Normal.([v -   ]))])
+
+end
