e83c3fe44339316ca088e7011524d9e13c0f767c
Nick Owens <owensnick@gmail.com>
2021-07-09 15:44:53 +0100

    Added MH LR jump sampler

--- src/sampling.jl
+++ src/sampling.jl
@@ -1,6 +1,6 @@
 
-function sample_mh_jump(model, d=180)
+function sample_mh_jump(model, samples=1000 ; δ=180, σ=5, p=0.2)
 
-    mh = MH(:startpos => x -> [MixtureModel(Normal.([v -   ]))])
-
+    mh = MH(:startpos => x -> [MixtureModel(Normal.([v -  δ, v + δ], σ), [p/2, 1-p, p/2]) for v in x])
+    sample(model, mh, samples)
 end
