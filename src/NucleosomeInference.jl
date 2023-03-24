7b310161f403871261eb120ef9f348ae0990cc1b
Nick Owens <owensnick@gmail.com>
2023-03-24 14:54:25 +0000

    setup module

--- src/NucleosomeInference.jl
+++ src/NucleosomeInference.jl
@@ -1,18 +1,6 @@
 module NucleosomeInference
 
 # Write your package code here.
-using Turing
-using Random
-using DataFrames
-using StatsFuns
-using Plots, StatsPlots, Measures
-
-export randclasses, simpletwoclass, plotconfig, simplemulticlass, sample_mh_jump
-
-include("sample.jl")
-include("plotting.jl")
-include("model.jl")
-include("sampling.jl")
 
 
 
