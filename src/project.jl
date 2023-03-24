5b6c18a43bf8023d6dd75893c4b0b2513415ee6c
Nick Owens <owensnick@gmail.com>
2023-03-24 14:54:46 +0000

    setup project

--- src/project.jl
+++ src/project.jl
@@ -4,9 +4,27 @@ using DataFrames
 using StatsFuns
 using Plots, StatsPlots, Measures
 
+using FASTX, BioSequences
+
 #export randclasses, simpletwoclass, plotconfig, simplemulticlass, sample_mh_jump
 
 include("sample.jl")
 include("plotting.jl")
 include("model.jl")
 include("sampling.jl")
+include("smf.jl")
+include("plots.jl")
+
+
+"""
+    getprojectdir()
+    function to determine the where the project directory is relative to current working directory which may be in a sub directory of the project dir
+"""
+function getprojectdir()
+    d = pwd()
+    if basename(d) == "notebooks"
+        return dirname(d)
+    else
+        return d
+    end
+end
